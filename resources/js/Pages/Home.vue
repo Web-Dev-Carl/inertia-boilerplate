333e3f4bc395111a6e32c3974885360150574b80
Web-Dev-Carl <veganwebdev@gmail.com>
2024-08-03 19:58:38 +0100

    Persistant layouts - no need to add layout component to page when layout is set in the export block

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,7 @@
 <template>
-    <Layout class="w-full">
-        <div class="px-5 mt-4">
-            <h1 class="text-xl tracking-wider mb-4">Hello</h1>
-        </div>
-    </Layout>
+    <div class="px-5 mt-4">
+        <h1 class="text-xl tracking-wider mb-4">Hello</h1>
+    </div>
 </template>
 
 <script>
@@ -12,7 +10,7 @@ import Layout from "../Shared/Layout.vue";
 
 export default {
     name: "Home",
-    components: {Layout}
+    layout: Layout
 }
 </script>
 
