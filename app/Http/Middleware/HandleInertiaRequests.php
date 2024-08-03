33a3fd4c6755d80aeecc73211dfdedcf26bedbe4
Web-Dev-Carl <veganwebdev@gmail.com>
2024-08-03 19:44:26 +0100

    Tidy up

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => [
                 'user' => [
-                    'username' => 'John Doe'
+                    'username' => 'John Doe' // Get with api in real world scenario
                 ]
             ]
         ]);
