558059b0eed6b0d5b431fc6e7de507186695db05
Web-Dev-Carl <veganwebdev@gmail.com>
2024-08-03 19:42:12 +0100

    Example of how to pass global props to any component - only pass essential data as will be passed on every request

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -2,6 +2,7 @@
     <header class="flex w-full">
         <Nav>
             <h1 class="font-bold text-lg">My App</h1>
+            <p>Welcome back {{ $page.props.auth.user.username }}</p>
         </Nav>
     </header>
     <div class="max-w-3xl mx-auto">
