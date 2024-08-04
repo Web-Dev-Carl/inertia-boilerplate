18cdd587c7f9dc5a5b9fee28fa72098b2edcdb59
Web-Dev-Carl <veganwebdev@gmail.com>
2024-08-04 03:51:02 +0100

    basic implementation of Auth

---
+++ resources/js/Shared/AuthLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="max-w-4xl mx-auto">
+        <slot/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "AuthLayout",
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -4,49 +4,55 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\User;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::get('login', [LoginController::class, 'create'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
 
-Route::get('/users', function (Request $request) { // Inject Request here
-    return Inertia::render('Users/Index', [
-        'users' => User::query()
-            ->when($request->input('search'), function ($query, $search) {
-                $query->where('name', 'LIKE', "%$search%");
-            })
-            ->paginate(10)
-            ->withQueryString()
-            ->through(fn($user) => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-            ]),
-        'filters' => $request->only(['search'])
-    ]);
-});
+Route::middleware('auth')->group(function () {
 
-Route::get('/users/create', function () {
-    return Inertia::render('Users/Create');
-});
+    Route::get('/', function () {
+        return Inertia::render('Home');
+    });
 
-Route::post('/users', function (Request $request) { // Inject Request here
-    $validate = $request->validate([ // Use the Request instance to call validate
-        'name' => 'required',
-        'email' => ['required', 'email'],
-        'password' => 'required',
-    ]);
+    Route::get('/users', function (Request $request) { // Inject Request here
+        return Inertia::render('Users/Index', [
+            'users' => User::query()
+                ->when($request->input('search'), function ($query, $search) {
+                    $query->where('name', 'LIKE', "%$search%");
+                })
+                ->paginate(10)
+                ->withQueryString()
+                ->through(fn($user) => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ]),
+            'filters' => $request->only(['search'])
+        ]);
+    });
 
-    User::create($validate);
+    Route::get('/users/create', function () {
+        return Inertia::render('Users/Create');
+    });
 
-    return redirect('/users');
-});
+    Route::post('/users', function (Request $request) { // Inject Request here
+        $validate = $request->validate([ // Use the Request instance to call validate
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+        User::create($validate);
+
+        return redirect('/users');
+    });
 
 
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
-});
+    Route::get('/settings', function () {
+        return Inertia::render('Settings');
+    });
 
-Route::post('/logout', function () {
-    return 'Logging user out';
+    Route::post('/logout', [LoginController::class, 'logout']);
+
 });
