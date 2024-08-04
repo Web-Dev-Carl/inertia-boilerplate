f702c8290f481aa1c4dca5429a4e4650090db410
Web-Dev-Carl <veganwebdev@gmail.com>
2024-08-04 01:57:51 +0100

    Filtering

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 use App\Models\User;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Home');
 });
 
-Route::get('/users', function () {
+Route::get('/users', function (Request $request) { // Inject Request here
     return Inertia::render('Users', [
-        'users' => User::paginate(10)->through(fn($user) => [
-            'id' => $user->id,
-            'name' => $user->name,
-            'email' => $user->email,
-        ])
+        'users' => User::query()
+            ->when($request->input('search'), function ($query, $search) {
+                $query->where('name', 'LIKE', "%$search%");
+            })
+            ->paginate(10)
+            ->withQueryString()
+            ->through(fn($user) => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ]),
+        'filters' => $request->only(['search'])
     ]);
 });
 
@@ -37,5 +34,3 @@ Route::get('/settings', function () {
 Route::post('/logout', function () {
     return 'Logging user out';
 });
-
-
