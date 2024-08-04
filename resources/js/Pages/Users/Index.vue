bca17f11accf798eedc2ad6cc7663c843ad0b0b8
Web-Dev-Carl <veganwebdev@gmail.com>
2024-08-04 03:12:00 +0100

    adding throtteling / debounce with lodash

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -101,7 +101,8 @@
 <script setup>
 import Paginator from "../../Components/Paginator.vue";
 import {ref, watch} from "vue";
-import {Inertia} from "@inertiajs/inertia"
+import {Inertia} from "@inertiajs/inertia";
+import debounce from "lodash/debounce";
 
 let props = defineProps({
     users: Object,
@@ -110,12 +111,12 @@ let props = defineProps({
 
 let search = ref(props.filters.search);
 
-watch(search, value => {
+watch(search, debounce(function (value) {
     Inertia.get('/users', {search: value}, {
         preserveState: true,
         replace: true, // prevents new history records
     });
-});
+}, 300));
 
 </script>
 
